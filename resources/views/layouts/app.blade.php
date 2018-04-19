6b41e38ee04108d57530212758fdd54d1e754ca6
Unknown <talles_of_destiny@yahoo.com.sg>
2018-04-20 01:26:34 +0900

    Added homepage (nav bar not yet)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,10 +68,72 @@
                 </div>
             </div>
         </nav>
-
+        <section id="header">
+            <div class="text-center">
+                <img src="{{ asset('images/logo.png') }}">
+            </div>
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="http://www.town.okutama.tokyo.jp//shared/images/main/gallery/image01.jpg">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Okutama 1</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="http://www.town.okutama.tokyo.jp//shared/images/main/gallery/image02.jpg">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Okutama 2</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="http://www.town.okutama.tokyo.jp//shared/images/main/gallery/image03.jpg">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Okutama 3</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </section>
         <main class="py-4">
             @yield('content')
         </main>
+        <section id="footer">
+            <div>
+                <div class="footer-padding">
+                    <div class="text-center">
+                        <img src="{{ asset('images/logo.png') }}">
+                    </div>
+                    <div class="text-center">
+                        <ul class="list-inline">
+                            <li class="list-inline-item"> <a href="#"><h2>Home</h2></a> </li>
+                            <li class="list-inline-item"> <a href="#"><h2>Facilities</h2></a> </li>
+                            <li class="list-inline-item"> <a href="#"><h2>About</h2></a> </li>
+                            <li class="list-inline-item"> <a href="#"><h2>Contact Us</h2></a> </li>
+                        </ul>
+                    </div>
+                    <div class="text-center">
+                        <h2>©2018年4月. All Right Reserved</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </body>
 </html>
